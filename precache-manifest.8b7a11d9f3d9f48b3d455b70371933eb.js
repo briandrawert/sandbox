47504f2488c69ec38c2f59901caa838110a5cc66
Brian Drawert <brian@drawert.net>
2020-08-02 12:28:39 -0400

    Updates

---
+++ precache-manifest.8b7a11d9f3d9f48b3d455b70371933eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ba58484b3c64628ace433316cf6b76",
+    "url": "/sandbox/index.html"
+  },
+  {
+    "revision": "d311ef39b5c4d41f4ee1",
+    "url": "/sandbox/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/sandbox/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sandbox/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d311ef39b5c4d41f4ee1",
+    "url": "/sandbox/static/js/main.528827f6.chunk.js"
+  },
+  {
+    "revision": "a8d3b63b586403d58b39",
+    "url": "/sandbox/static/js/runtime-main.a15f32b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sandbox/static/media/logo.5d5d9eef.svg"
+  }
+]);
