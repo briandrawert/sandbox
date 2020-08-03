805d02ab2d1ce646c4beb2ee5c059e1ca3b2db02
Brian Drawert <brian@drawert.net>
2020-08-03 10:02:45 -0400

    Updates

---
+++ TSU_Sandbox_Datafiles/make_index.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Build index from directory listing
+
+make_index.py </path/to/directory> [--header <header text>]
+Ref: https://stackoverflow.com/questions/39048654/how-to-enable-directory-indexing-on-github-pages
+"""
+
+INDEX_TEMPLATE = r"""
+<html>
+<body>
+<h2>${header}</h2>
+<p>
+% for name in names:
+    <li><a href="${name}">${name}</a></li>
+% endfor
+</p>
+</body>
+</html>
+"""
+
+EXCLUDED = ['index.html', 'make_index.py']
+
+import os
+import argparse
+
+# May need to do "pip install mako"
+from mako.template import Template
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory")
+    parser.add_argument("--header")
+    args = parser.parse_args()
+    fnames = [fname for fname in sorted(os.listdir(args.directory))
+              if fname not in EXCLUDED]
+    header = (args.header if args.header else os.path.basename(args.directory))
+    with open('index.html', 'w+') as fd:
+        fd.write(Template(INDEX_TEMPLATE).render(names=fnames, header=header))
+
+
+if __name__ == '__main__':
+    main()
