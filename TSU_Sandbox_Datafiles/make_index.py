c9e680e473a9e0079dc531cbd4016f1bca6e1c21
Brian Drawert <brian@drawert.net>
2020-08-03 11:42:09 -0400

    Updates

--- TSU_Sandbox_Datafiles/make_index.py
+++ TSU_Sandbox_Datafiles/make_index.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """ Build index from directory listing
 
-make_index.py </path/to/directory> [--header <header text>]
+make_index.py 
 Ref: https://stackoverflow.com/questions/39048654/how-to-enable-directory-indexing-on-github-pages
 """
 
 INDEX_TEMPLATE = r"""
 <html>
 <body>
-<h2>${header}</h2>
 <p>
 % for name in names:
     <li><a href="${name}">${name}</a></li>
@@ -18,25 +17,56 @@ INDEX_TEMPLATE = r"""
 </html>
 """
 
-EXCLUDED = ['index.html', 'make_index.py']
+JSON_TEMPLATE = r"""
+{
+[
+% for name in names:
+   { "name":"${name}" },
+% endfor
+]
+"""
+
+EXCLUDED = ['index.html', 'make_index.py', 'index.json']
 
 import os
 import argparse
 
 # May need to do "pip install mako"
 from mako.template import Template
+import json
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("directory")
-    parser.add_argument("--header")
-    args = parser.parse_args()
-    fnames = [fname for fname in sorted(os.listdir(args.directory))
+    fnames = [fname for fname in sorted(os.listdir("."))
               if fname not in EXCLUDED]
-    header = (args.header if args.header else os.path.basename(args.directory))
     with open('index.html', 'w+') as fd:
-        fd.write(Template(INDEX_TEMPLATE).render(names=fnames, header=header))
+        fd.write(Template(INDEX_TEMPLATE).render(names=fnames))
+
+    data = {
+        'national' : [],
+        'regions' : [],
+        'state' : [],
+    }
+    for fn in fnames:
+        print(fn)
+        parts = fn.split('_')
+        for ft in data.keys():
+            print(ft)
+            if fn.startswith(ft):
+                print("\tfound")
+                parts[0] = parts[0].replace(ft,'')
+                data[ft].append({
+                    "name": fn,
+                    "type": parts[0],
+                    "start": int(parts[1]),
+                    "end": int(parts[2]),
+                })
+                break;
+        
+
+    with open('index.json', 'w+') as fd:
+        #fd.write(Template(JSON_TEMPLATE).render(names=fnames))
+        fd.write(json.dumps(data))
 
 
 if __name__ == '__main__':
